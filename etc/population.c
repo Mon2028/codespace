93bc7783bec06394f37cb12a429cc2798c4280f8
Mon2028 <124736484+Mon2028@users.noreply.github.com>
2023-03-05 21:48:44 +0000

    Sun, Mar 5, 2023, 9:48 PM +00:00

---
+++ etc/population.c
@@ -0,0 +1,27 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int i;
+    do
+    {
+        i = get_int("Start population: \n");
+    }
+    while (i < 9);
+
+    int j;
+    do
+    {
+        j = get_int("End population: \n");
+    }
+    while (j < i);
+
+    int years = 0;
+    while (i < j)
+    {
+        i = i + (i / 3) - (i / 4);
+        years++;
+    }
+    printf("Years: %d\n", years);
+
