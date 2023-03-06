c306e1c277637f03e632611fc6e2e9f6f6bfa96f
Mon2028 <124736484+Mon2028@users.noreply.github.com>
2023-03-06 21:06:30 +0000

    Mon, Mar 6, 2023, 9:06 PM +00:00

--- hello/mario-less/mario.c
+++ hello/mario-less/mario.c
@@ -1,7 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+void build(int n);
+
+int main(void)
 {
+    int height;
+
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
     
+    build(height);
 }
