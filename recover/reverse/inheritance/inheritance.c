2fb0a50059bf3041d30a6685ce6d98b528421960
Mon2028 <124736484+Mon2028@users.noreply.github.com>
2023-09-14 02:48:51 +0000

    Thu, Sep 14, 2023, 2:48 AM +00:00

--- recover/reverse/inheritance/inheritance.c
+++ recover/reverse/inheritance/inheritance.c
@@ -6,30 +6,28 @@
 typedef struct person
 
 {
+
     struct person *parents[2];
     char alleles[2];
+
 }
 
 person;
-
 const int GENERATIONS = 3;
 const int INDENT_LENGTH = 4;
-
 person *create_family(int generations);
 void print_family(person *p, int generation);
 void free_family(person *p);
 char random_allele();
-
 int main(void)
 
 {
+
     srand(time(0));
-
     person *p = create_family(GENERATIONS);
-
     print_family(p, 0);
-
     free_family(p);
+
 }
 
 person *create_family(int generations)
@@ -40,79 +38,106 @@ person *create_family(int generations)
     if (n == NULL)
 
     {
+
         return NULL;
+
     }
 
     if (generations > 1)
 
     {
+
         n->parents[0] = create_family(generations - 1);
         n->parents[1] = create_family(generations - 1);
-
         n->alleles[0] = n->parents[0]->alleles[rand() % 2];
         n->alleles[1] = n->parents[1]->alleles[rand() % 2];
+
     }
 
     else
 
     {
+
         n->parents[0] = NULL;
         n->parents[1] = NULL;
-
         n->alleles[0] = random_allele();
         n->alleles[1] = random_allele();
+
     }
 
     return n;
 
 }
+
 void free_family(person *p)
+
 {
 
     if (p == NULL){
         return;
 
     }
+
     free_family(p->parents[0]);
     free_family(p->parents[1]);
     free(p);
+
 }
 
 void print_family(person *p, int generation)
 
 {
+
     if (p == NULL)
+
     {
 
         return;
 
     }
+
     for (int i = 0; i < generation * INDENT_LENGTH; i++)
+
     {
 
         printf(" ");
 
     }
+
     printf("Generation %i, blood type %c%c\n", generation, p->alleles[0], p->alleles[1]);
     print_family(p->parents[0], generation + 1);
     print_family(p->parents[1], generation + 1);
+
 }
 
 char random_allele()
 
 {
+
     int r = rand() % 3;
     if (r == 0)
+
     {
+
         return 'A';
+
     }
+
     else if (r == 1)
+
     {
+
         return 'B';
+
     }
+
     else
+
     {
+
         return 'O';
+
     }
+
 }
 
