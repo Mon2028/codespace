1c6fc3553f9428017153644983b3a4e84ba66463
Mon2028 <124736484+Mon2028@users.noreply.github.com>
2023-11-19 07:43:03 +0000

    Sun, Nov 19, 2023, 7:43 AM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -4,23 +4,18 @@ import re
 
 if len(sys.argv) != 3:
     sys.exit("Usage: python dna.py data.csv sequence.txt")
-
 database = sys.argv[1]
 sequence = sys.argv[2]
 db = []
-
 def main():
     with open(database) as file:
         reader = csv.DictReader(file)
         for row in reader:
             db.append(row)
-
     with open(sequence) as txt:
         dna = txt.read()
         mysequence = dna
-
-    text = mysequence
-
+text = mysequence
     AGATC = compute('AGATC', text)
     AATG = compute('AATG', text)
     TATC = compute('TATC', text)
@@ -29,24 +24,22 @@ def main():
     GATA = compute('GATA', text)
     GAAA = compute('GAAA', text)
     TCTG = compute('TCTG', text)
-
     if sys.argv[1] == 'databases/small.csv':
         for i in range(len(db)):
             for i in range(len(db)):
                 if all([db[i]["AGATC"] == str(AGATC), db[i]["AATG"] == str(AATG), db[i]["TATC"] == str(TATC)]):
-                    output = db[i]["Name"]
+                    output = db[i]["name"]
                     break
                 else:
-                    output = "No Match"
+                    output = "No match"
     else:
         for i in range(len(db)):
             if all([db[i]["AGATC"] == str(AGATC), db[i]["TTTTTTCT"] == str(TTTTTTCT), db[i]["TCTAG"] == str(TCTAG), db[i]["AATG"] == str(AATG),
                     db[i]["GATA"] == str(GATA), db[i]["TATC"] == str(TATC), db[i]["GAAA"] == str(GAAA), db[i]["TCTG"] == str(TCTG)]):
-                output = db[i]["Name"]
+                output = db[i]["name"]
                 break
             else:
-                output = "No Match"
-
+                output = "No match"
     print(output)
 
 def compute(pattern, dnaSeq):
