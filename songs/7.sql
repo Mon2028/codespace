970379bb9c095534850a6827bb25c777ee230b9f
Mon2028 <124736484+Mon2028@users.noreply.github.com>
2023-11-19 21:27:53 +0000

    Sun, Nov 19, 2023, 9:27 PM +00:00

--- songs/7.sql
+++ songs/7.sql
@@ -1,1 +1,1 @@
-select avg(energy) from where songs.artist_id = (select id from artists where name = "Drake")
+select avg(all energy) from where songs.artist_id = (select id from artists where name = "Drake")
