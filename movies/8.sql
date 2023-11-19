2b1560ec8e6fc7d32594837b068b7ee6dd8e2060
Mon2028 <124736484+Mon2028@users.noreply.github.com>
2023-11-19 22:25:47 +0000

    Sun, Nov 19, 2023, 10:25 PM +00:00

--- movies/8.sql
+++ movies/8.sql
@@ -1,8 +1,8 @@
-select name from people, movies, stars
-where people.id = stars.person_id
-and stars.movie_id = movies.id
+select name from people, movies, stars;
+where people.id = stars.person_id;
+and stars.movie_id = movies.id;
 and title = "Toy Story";
-select name from people
-join stars on people.id = stars.person_id
-join movies on stars.movie_id = movies.id
+select name from people;
+join stars on people.id = stars.person_id;
+join movies on stars.movie_id = movies.id;
 where title = "Toy Story";
