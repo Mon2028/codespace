26fef91c85ce590d3479c077b1a8669b4f1e7517
Mon2028 <124736484+Mon2028@users.noreply.github.com>
2023-11-20 07:08:30 +0000

    Mon, Nov 20, 2023, 7:08 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -37,7 +37,7 @@ def index():
     """Show portfolio of stocks"""
     user_id = session["user_id"]
 
-    transactions_db = db.execute("SELECT symbol, SUM(shares) AS shares, price FROM transactions WHERE user_id = ?")
+    transactions_db = db.execute("SELECT symbol, SUM(shares) AS shares, price FROM transactions WHERE user_id = ? GROUP BY symbol")
     cash_db = db.execute("SELECT cash FROM users WHERE id = ?", user_id)
     cash = cash_db[0]["cash"]
 
