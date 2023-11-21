52c12f484ec9cad0ac08479dda6c1ba2a903663b
Mon2028 <124736484+Mon2028@users.noreply.github.com>
2023-11-21 01:20:08 +0000

    Tue, Nov 21, 2023, 1:20 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -95,7 +95,10 @@ def buy():
 @login_required
 def history():
     """Show history of transactions"""
-    
+
+    transactions = db.execute("SELECT * FROM transactions WHERE user_id = :user_id ORDER BY timestamp DESC", user_id=session["user_id"])
+
+    return render_template("history.html", transactions=transactions)
 
 
 @app.route("/login", methods=["GET", "POST"])
