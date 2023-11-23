6b184599c1bcd5e23d48bcb7e0fcc0c2584f865b
Mon2028 <124736484+Mon2028@users.noreply.github.com>
2023-11-23 07:29:08 +0000

    Thu, Nov 23, 2023, 7:29 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -44,37 +44,49 @@ int main(int argc, string argv[])
         return 1;
     }
 
+    // Populate array of candidates
     candidate_count = argc - 1;
     if (candidate_count > MAX_CANDIDATES)
     {
         printf("Maximum number of candidates is %i\n", MAX_CANDIDATES);
         return 2;
+
     }
+
     for (int i = 0; i < candidate_count; i++)
+
     {
+
         candidates[i].name = argv[i + 1];
         candidates[i].votes = 0;
         candidates[i].eliminated = false;
+
     }
 
     voter_count = get_int("Number of voters: ");
     if (voter_count > MAX_VOTERS)
+
     {
+
         printf("Maximum number of voters is %i\n", MAX_VOTERS);
         return 3;
+
     }
 
-    // Keep querying for votes
     for (int i = 0; i < voter_count; i++)
+
     {
 
         for (int j = 0; j < candidate_count; j++)
+
         {
+
             string name = get_string("Rank %i: ", j + 1);
             if (!vote(i, j, name))
+
             {
 
-                printf("Vote is not valid.\n");
+                printf("Invalid vote.\n");
                 return 4;
 
             }
@@ -99,7 +111,7 @@ int main(int argc, string argv[])
 
         }
 
-        int least = find_min();
+        int min = find_min();
         bool tie = is_tie(min);
         if (tie)
 
@@ -123,7 +135,7 @@ int main(int argc, string argv[])
 
         }
 
-        eliminate(least);
+        eliminate(min);
         for (int i = 0; i < candidate_count; i++)
 
         {
@@ -170,28 +182,39 @@ void tabulate(void)
     for (int i = 0; i < voter_count; i++)
 
     {
+
         for (int j = 0; j < candidate_count; j++)
+
         {
+
             if (candidates[preferences[i][j]].eliminated == false)
+
             {
+
                 candidates[preferences[i][j]].votes += 1;
                 break;
+
             }
+
         }
 
     }
 
     return;
+
 }
 
-// Print the winner of the election, if there is one
 bool print_winner(void)
+
 {
-    // TODO
+
     for (int i = 0; i < candidate_count; i++)
+
     {
+
         string most = candidates[i].name;
         if (candidates[i].votes > voter_count / 2)
+
         {
 
             printf("%s\n", most);
@@ -208,6 +231,7 @@ bool print_winner(void)
 int find_min(void)
 
 {
+
     int minvotes = voter_count;
     for (int i = 0; i < candidate_count; i++)
 
