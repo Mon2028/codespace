e662bc462547cf2efcbe47a226fb917a418990ca
Mon2028 <124736484+Mon2028@users.noreply.github.com>
2023-11-23 06:49:13 +0000

    Thu, Nov 23, 2023, 6:49 AM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -65,13 +65,35 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(candidates[i].name, name) = 0)
+        {
+            candidates[i].votes++;
+            return true;
+        }
+    }
     return false;
 }
 
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    // TODO
+    int maximum_vote = 0;
+
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes > maximum_vote)
+        {
+            maximum_vote = candidates[i].votes;
+        }
+    }
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes = maximum_vote)
+        {
+            printf("%s/n", candidates[i].name);
+        }
+    }
     return;
 }
