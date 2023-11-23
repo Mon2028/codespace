4fc94ce9a66e801cc138e21f02960734e5fc328f
Mon2028 <124736484+Mon2028@users.noreply.github.com>
2023-11-23 07:10:14 +0000

    Thu, Nov 23, 2023, 7:10 AM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -12,26 +12,23 @@ typedef struct
     int votes;
 } candidate;
 
-// Array of candidates
 candidate candidates[MAX];
 
 // Number of candidates
 int candidate_count;
 
-// Function prototypes
 bool vote(string name);
 void print_winner(void);
 
 int main(int argc, string argv[])
 {
-    // Check for invalid usage
+
     if (argc < 2)
     {
         printf("Usage: plurality [candidate ...]\n");
         return 1;
     }
 
-    // Populate array of candidates
     candidate_count = argc - 1;
     if (candidate_count > MAX)
     {
@@ -46,32 +43,68 @@ int main(int argc, string argv[])
 
     int voter_count = get_int("Number of voters: ");
 
-    // Loop over all voters
     for (int i = 0; i < voter_count; i++)
     {
         string name = get_string("Vote: ");
 
-        // Check for invalid vote
         if (!vote(name))
         {
             printf("Invalid vote.\n");
         }
     }
 
-    // Display winner of election
     print_winner();
 }
 
-// Update vote totals given a new vote
 bool vote(string name)
 {
-    // TODO
+
+    for (int i = 0; i < candidate_count; i++){
+        if(strcmp(candidates[i].name, name) == 0){
+            candidates[i].votes += 1;
+            return true;
+
+        }
+
+    }
+
     return false;
+
 }
 
-// Print the winner (or winners) of the election
 void print_winner(void)
+
 {
-    // TODO
-    return;
+
+    int maximum_votes = 0;
+    for (int i = 0; i < candidate_count; i++)
+
+    {
+
+        if (candidates[i].votes > maximum_votes)
+
+        {
+
+            maximum_votes = candidates[i].votes;
+
+        }
+
+    }
+
+    for (int i = 0; i < candidate_count; i++)
+
+    {
+
+        if (candidates[i].votes == maximum_votes)
+
+        {
+
+            printf("%s\n", candidates[i].name);
+
+        }
+
+    }
+
+   return;
+
 }
